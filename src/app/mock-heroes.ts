5c2732221ad0d086ba03696f7a09ea781b1b2f81
Uriegas <1930526@upv.edu.mx>
2021-02-19 18:59:05 -0600

    Heroes:

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Dr, Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Magneta'},
+    {id: 15, name: 'Liz'},
+    {id: 16, name: 'Dr IQ'},
+    {id: 17, name: 'El tronco'},
+    {id: 18, name: 'Maradona'},
+    {id: 19, name: 'Dieguito'},
+    {id: 20, name: 'La Chicana'},
+]
